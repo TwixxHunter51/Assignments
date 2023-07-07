465a0b86d446617d5b3af3e8d747ba2237b6b90c
TwixxHunter51 <138794228+TwixxHunter51@users.noreply.github.com>
2023-07-07 04:28:23 -0400

    Add files via upload

---
+++ Question 1/Question 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // Print Hello World
+            Console.ReadKey();                 // Do not close the window until user presses a key
+        }
+    }
+}
