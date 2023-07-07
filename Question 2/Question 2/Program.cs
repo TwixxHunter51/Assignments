71cedbd0614ba9146e40d8f927dc28aeb70a40d6
TwixxHunter51 <138794228+TwixxHunter51@users.noreply.github.com>
2023-07-07 04:29:25 -0400

    Add files via upload

---
+++ Question 2/Question 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name");    // Ask for user's name
+           String name =  Console.ReadLine();               // Scan for a string that user has entered and store it in the variable 'name'
+            Console.WriteLine("Hello " + name);             // Print Hello with the entered name
+            Console.ReadKey();                              // Do not close the window until a key is pressed
+        }
+    }
+}
