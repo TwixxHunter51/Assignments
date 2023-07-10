1861c872661319019ea6710f0269c3ef936330ff
Ahmed Tariq <138794228+TwixxHunter51@users.noreply.github.com>
2023-07-10 12:27:39 +0500

    Add files via upload

---
+++ ArrayPracticeQuestion/ArrayPracticeQuestion/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace ArrayPracticeQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "hashim";
+
+            for (int i = 0; i < userName.Length; i++)
+            {
+                Console.WriteLine(userName[i]);
+            }
+
+            // AddFiveNumbersUsingVariables();
+            //Console.WriteLine("---------------------------------------------------------");
+            //Console.WriteLine("---------------------------------------------------------");
+            //Console.WriteLine("---------------------------------------------------------");
+            //AddFiveNumbersUsingArray();
+
+
+            //int[] numbers = new int[5]; // Declare and initialize an integer array with 5 elements
+
+            //numbers[0] = 10; // Assign value 10 to the first element
+            //numbers[1] = 20; // Assign value 20 to the second element
+
+            //Console.WriteLine(numbers[0]); // Output: 10
+            //Console.WriteLine(numbers[1]); // Output: 20
+
+
+            //string[] fruits = { "Apple", "Banana", "Orange", "Mango" };
+
+            //// Using a for loop to iterate through the array
+            //for (int i = 0; i < fruits.Length; i++)
+            //{
+            //    Console.WriteLine(fruits[i]);
+            //}
+
+            //// Using a foreach loop to iterate through the array
+            //foreach (string fruit in fruits)
+            //{
+            //    Console.WriteLine(fruit);
+            //}
+
+
+            //int[] numbersArray = { 10, 5, 20, 15, 25 };
+            //SumOfArray(numbersArray);
+
+
+            Console.ReadKey();
+        }
+
+        static void AddFiveNumbersUsingVariables()
+        {
+            int numberOne;
+            int numberTwo;
+            int numberThree;
+            int numberFour;
+            int numberFive;
+
+            Console.WriteLine("Enter first number: ");
+            numberOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter three number: ");
+            numberThree = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter four number: ");
+            numberFour = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter five number: ");
+            numberFive = int.Parse(Console.ReadLine());
+
+            int sum = numberOne + numberTwo + numberThree + numberFour + numberFive;
+
+            Console.WriteLine("Sum of the Five Integers is: " + sum);
+        }
+
+        static void AddFiveNumbersUsingArray()
+        {
+            int sum = 0;
+            int[] numbers = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Enter the " + (i+1) + " number: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+                sum += numbers[i]; // where sum += numbers[i] means sum = sum + numbers[i]
+            }
+            Console.WriteLine("Sum of All Integers is: " + sum);
+        }
+
+        static void SumOfArray(int[] numbers)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num; // Add each number to the sum
+            }
+            Console.WriteLine("The sum of all numbers is: " + sum); // Output: 75
+        }
+    }
+}
---
+++ OOP/School Management System - OPP.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace School_Management_System___OPP
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Dob { get; set; }
+        public string Address { get; set; }
+
+        public Person(string name, DateTime dob, string address)
+        {
+            Name = name;
+            Dob = dob;
+            Address = address;
+        }
+    }
+    public class Student : Person
+    {
+        public string Grade { get; set; }
+        public Teacher ClassTeacher { get; set; }
+        public Student(string name, DateTime dob, string address, string grade) : base(name, dob, address)
+        {
+            Grade = grade;
+        }
+        public void AssignTeacher(Teacher teacher)
+        {
+            ClassTeacher = teacher;
+            Console.WriteLine(teacher.Name + " teacher assigned to the student " + this.Name);
+        }
+    }
+    public class Teacher : Person
+    {
+        public string Subject { get; set; }
+        public Teacher(string name, DateTime dob, string address, string subject) : base(name, dob, address)
+        {
+            Subject = subject;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Now you can create instances of students and teachers, and assign teachers to students:
+            
+            Teacher mrSmith = new Teacher("Mr. Smith", new DateTime(1980, 1, 1), "123 St", "Maths");
+            Student john = new Student("John", new DateTime(2010, 1, 1), "456 St", "5");
+
+            john.AssignTeacher(mrSmith);
+
+            Console.ReadKey();
+        }
+    }
+}
